84c8349796ac6d1a388382abfdf43b5d4ff0e51e
KangWon LEE <kangwon.lee@tukorea.ac.kr>
2022-06-17 15:18:38 +0900

    first table

--- sieve.py
+++ sieve.py
@@ -1,8 +1,44 @@
+# ref : https://m.blog.naver.com/dsz08082/22174314569
+
 import tkinter as tk
+import tkinter.ttk as ttk
 
 
 def main():
-    pass
+
+    m = 3
+    n = 10
+
+    width = 100
+
+    root = tk.Tk()
+    root.title("Erasthotenes Sieve")
+    root.geometry(f"{width*n}x{100*m}+100+100")
+    root.resizable(False, False)
+
+    columns = [f"col{i:02d}" for i in range(10)]
+
+    treeview = ttk.Treeview(
+        root,
+        columns=columns,
+        displaycolumns=columns,
+    )
+    treeview.pack()
+
+    for col in range(10):
+        treeview.column(f"#{col}", width=width)
+        treeview.heading(f"#{col}", text="")
+
+    treelist = [
+        tuple(range( 1, 10+1)),
+        tuple(range(11, 20+1)),
+        tuple(range(21, 30+1)),
+    ]
+
+    for i, row in enumerate(treelist):
+        treeview.insert('', 'end', text='', values=row, iid=f"row{i:02d}")
+
+    root.mainloop()
 
 
 if "__main__" == __name__:
